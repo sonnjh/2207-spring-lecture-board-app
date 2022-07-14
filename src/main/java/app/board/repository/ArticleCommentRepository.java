0a79dfa847b2c8c1282395bcc9f0059447f7c4d6
sonnjh <jaeha.sonn@gmail.com>
2022-07-15 03:02:55 +0900

    #7 - 리포지토리 어노테이션 삭제

--- src/main/java/app/board/repository/ArticleCommentRepository.java
+++ src/main/java/app/board/repository/ArticleCommentRepository.java
@@ -2,8 +2,6 @@ package app.board.repository;
 
 import app.board.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
--- src/main/java/app/board/repository/ArticleRepository.java
+++ src/main/java/app/board/repository/ArticleRepository.java
@@ -2,8 +2,6 @@ package app.board.repository;
 
 import app.board.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
